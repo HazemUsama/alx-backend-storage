23014a32fb7947f9bc83303ea944e5869a931404
HazemUsama <hazemosama681@gmail.com>
2024-06-14 05:29:16 +0300

    another missing ;

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -11,8 +11,8 @@ BEGIN
   IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0 THEN
     INSERT INTO projects (name) VALUES (project_name);
   END IF;
-  SET project_id = (SELECT id FROM projects WHERE name = project_name);
-  INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score)
+  SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
+  INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
 END!!
 
 DELIMITER ;
