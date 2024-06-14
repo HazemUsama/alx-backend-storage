04dbd7bdd883648b5390ddfc67aedeae40cdbe60
HazemUsama <hazemosama681@gmail.com>
2024-06-14 04:50:28 +0300

    Before update

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,9 @@
+-- Resets the attribute valid_email only when the email has been changed.
+CREATE TRIGGER no_validation
+BEFORE UPDATE ON users
+FOR EACH ROW
+  BEGIN
+    IF NEW.email <> OLD.email THEN
+      SET valid_email = 0
+    END IF
+  END
