fb7b84e79389ac77ae299f4e03464839f61619b7
HazemUsama <hazemosama681@gmail.com>
2024-06-19 04:46:56 +0300

    Fixes

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -9,10 +9,11 @@ if __name__ == "__main__":
     """
     client = MongoClient('mongodb://127.0.0.1:27017')
     log_collection = client.logs.nginx
-    print(log_collection.count_documents({}))
+    print(log_collection.count_documents({}), 'logs')
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
+    print('Methods:')
     for method in methods:
         method_cound = log_collection.count_documents({"method": method})
         print('\tmethod {}: {}'.format(method, method_cound))
