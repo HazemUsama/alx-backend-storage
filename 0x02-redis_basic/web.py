49e56ef250e9e28fe86560041ca05d4e98a58307
HazemUsama <hazemosama681@gmail.com>
2024-06-20 05:46:51 +0300

    Web cache

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Implementing an expiring web cache and tracker"""
+import redis
+import requests
+from funtools import wraps
+from typing import Callable
+
+r = redis.Redis()
+
+
+def count_requests(method: Callable) -> Callable:
+    """Decorator to count requests"""
+    @wraps(method)
+    def wrapper(self, url):
+        """Wrapper function"""
+        r.incr(f"count:{url}")
+        result = r.get(url)
+        if result:
+            return result.decode('utf-8')
+        result = method(self, url)
+        r.setex(url, 10, result)
+        return result
+    return wrapper
+
+
+def get_page(url: str) -> str:
+    """Get page"""
+    result = requests.get(url).text
+    return result
