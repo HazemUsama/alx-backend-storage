022319fd38816bb854c3b05a370c6b5009eadaf1
HazemUsama <hazemosama681@gmail.com>
2024-06-20 05:31:59 +0300

    Replay system

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,6 +6,20 @@ from typing import Union, Optional, Callable
 from functools import wraps
 
 
+def replay(method: Callable) -> None:
+    """Replay decorator"""
+    name = method.__qualname__
+    inputs = f"{name}:inputs"
+    outputs = f"{name}:outputs"
+    cache = method.__self__._redis
+    count = cache.get(name).decode('utf-8')
+    inputs_list = cache.lrange(inputs, 0, -1)
+    outputs_list = cache.lrange(outputs, 0, -1)
+    print(f"{name} was called {count} times:")
+    for i, o in zip(inputs_list, outputs_list):
+        print(f"{name}(*{i.decode('utf-8')}) -> {o.decode('utf-8')}")
+
+
 def count_calls(method: Callable) -> Callable:
     """Count calls decorator"""
     @wraps(method)
@@ -21,9 +35,12 @@ def call_history(method: Callable) -> Callable:
     @wraps(method)
     def wrapper(self, *args, **kwargs):
         """Wrapper function"""
-        self._redis.rpush(f"{method.__qualname__}:inputs", str(args))
+        name = method.__qualname__
+        inputs = f"{name}:inputs"
+        outputs = f"{name}:outputs"
+        self._redis.rpush(inputs, str(args))
         data = method(self, *args, **kwargs)
-        self._redis.rpush(f"{method.__qualname__}:outputs", data)
+        self._redis.rpush(outputs, data)
         return data
     return wrapper
 
