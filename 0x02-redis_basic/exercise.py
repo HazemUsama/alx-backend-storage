4fec24f1ce145ec1c113487b35e7430d65a2e027
HazemUsama <hazemosama681@gmail.com>
2024-06-20 05:17:19 +0300

    call_history wrapper

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,6 +16,18 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """Call history decorator"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper function"""
+        self._redis.rpush(f"{method.__qualname__}:inputs", str(args))
+        data = method(self, *args, **kwargs)
+        self._redis.rpush(f"{method.__qualname__}:outputs", data)
+        return data
+    return wrapper
+
+
 class Cache:
     """Cache class"""
 
@@ -25,6 +37,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in redis"""
         key = str(uuid.uuid4())
